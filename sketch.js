91ac72747cd17f919acb816f5121c098eec2bd59
monika-bagla <monika.bagla@gmail.com>
2021-03-21 21:05:33 +0530

    monika

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform=new Ground(100,height,500,20)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -38,7 +39,7 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    background("red");
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
